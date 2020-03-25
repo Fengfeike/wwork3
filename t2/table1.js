ff2d259c0c839a527da0ef8a1652d531d31c3887
Fengfeike <2088399108@qq.com>
2020-03-25 11:01:52 +0800

    1

---
+++ t2/table1.js
@@ -0,0 +1,35 @@
+
+$(function(){
+  var myChart = echarts.init($('.main').get(0));
+  var xData = [2000,2005,2010,2015,2020];
+  var yData = [0,2,4,6,8,10];
+
+  var option ={
+    title:{
+      text:'JavaScript语言排名变化'
+    },
+    tooltip:{
+      trigger:'axis',
+      formatter:function(param){
+        return '排名<br>' + param[0].name + ":" + param[0].value
+      },
+    },
+    legend:{
+      data:['排名']
+    },
+    xAxis:{
+      type:'category',
+      data:xData
+      },
+    yAxis:{
+      type:'value',
+      data:yData
+    },
+    series:[{
+      
+      type:'line',
+      data:[6,9,8,8,7]
+    }]
+  };
+  myChart.setOption(option);
+})
